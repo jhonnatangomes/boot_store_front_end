659d508c9fad02818abaa7a80d74748c6ec900c5
jhonnatangomes <57162073+jhonnatangomes@users.noreply.github.com>
2021-11-10 19:13:34 -0300

    Update dataApi.js

--- src/services/dataApi.js
+++ src/services/dataApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosBase = axios.create({
-	baseURL: 'http://localhost:4000/',
+	baseURL: 'https://boot-store.herokuapp.com/',
 });
 
 const getServerStatus = () => axiosBase.get('/status');
