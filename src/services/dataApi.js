478a3827fa3000412899ec25501818f5d693a104
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-11-10 19:25:21 -0300

    created environment variable for api base URL

--- src/services/dataApi.js
+++ src/services/dataApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosBase = axios.create({
-    baseURL: 'http://localhost:4000/',
+    baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 const getServerStatus = () => axiosBase.get('/status');
