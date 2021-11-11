ae6103aa12757a4d9fdd22d123a7f9cf15332820
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-11-11 12:43:06 -0300

    added redirect to home for non-existent routes

--- src/App.js
+++ src/App.js
@@ -11,46 +11,48 @@ import Product from './components/productPage/Product';
 import Cart from './components/cart/Cart';
 import UserOrders from './components/userOrders/UserOrders';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { useState } from 'react';
 
 import UserContext from './contexts/UserContext';
 
 export default function App() {
-	const [user, setUser] = useState(null);
+    const [user, setUser] = useState(null);
 
-	return (
-		<BrowserRouter>
-			<UserContext.Provider value={{ user, setUser }}>
-				<Header />
-				<PageContainer>
-					<Switch>
-						<Route exact path={routes.login}>
-							<Login />
-						</Route>
+    return (
+        <BrowserRouter>
+            <UserContext.Provider value={{ user, setUser }}>
+                <Header />
+                <PageContainer>
+                    <Switch>
+                        <Route exact path={routes.login}>
+                            <Login />
+                        </Route>
 
-						<Route path={routes.userOrders}>
-							<UserOrders />
-						</Route>
+                        <Route exact path={routes.userOrders}>
+                            <UserOrders />
+                        </Route>
 
-						<Route exact path={routes.cart}>
-							<Cart />
-						</Route>
+                        <Route exact path={routes.cart}>
+                            <Cart />
+                        </Route>
 
-						<Route path={routes.products}>
-							<Product />
-						</Route>
+                        <Route exact path={routes.products}>
+                            <Product />
+                        </Route>
 
-						<Route exact path={routes.categories}>
-							<Categories />
-						</Route>
+                        <Route exact path={routes.categories}>
+                            <Categories />
+                        </Route>
 
-						<Route path={routes.home}>
-							<Home />
-						</Route>
-					</Switch>
-				</PageContainer>
-			</UserContext.Provider>
-		</BrowserRouter>
-	);
+                        <Route exact path={routes.home}>
+                            <Home />
+                        </Route>
+
+                        <Redirect to={routes.home} />
+                    </Switch>
+                </PageContainer>
+            </UserContext.Provider>
+        </BrowserRouter>
+    );
 }
