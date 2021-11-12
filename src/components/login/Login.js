f08f2d711b8ca57bff945e94b46c1f6880995183
Nello-Moreira <g.moreira1@outlook.com>
2021-11-12 19:00:28 -0300

    implemented: login layout

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,17 +1,88 @@
-import { useHistory } from 'react-router-dom';
-import { useContext } from 'react';
+import { useHistory } from 'react-router';
+import { useState, useContext } from 'react';
 import UserContext from '../../contexts/UserContext';
 
 import routes from '../../routes/routes';
 
-export default function Login() {
-	const { setUser } = useContext(UserContext);
+import FormContainer from '../containers/FormContainer';
+import FormTitle from '../inputsAndForms/FormTitle';
+import FakeLink from '../FakeLink';
+import FormStyle from '../inputsAndForms/FormStyle';
+import InputLabel from '../inputsAndForms/InputLabel';
+import InputStyle from '../inputsAndForms/InputStyle';
+import FormButton from '../buttons/FormButton';
+
+import { getServerStatus } from '../../services/dataApi';
+
+export default function SignUp() {
 	const history = useHistory();
+	const [loading, setLoading] = useState(false);
+	const [inputs, setInputs] = useState({
+		email: '',
+		password: '',
+	});
+
+	function inputModifier(field, newValue) {
+		if (loading) return;
+
+		inputs[field] = newValue;
+		setInputs({ ...inputs });
+	}
+
+	function formSubmit(event) {
+		event.preventDefault();
+
+		if (loading) return;
+
+		setLoading(true);
+
+		getServerStatus().then(() => {
+			console.log('hey');
+			setLoading(false);
+		});
+	}
 
 	return (
-		<>
-			<div onClick={() => setUser('teste')}>Login</div>
-			<div onClick={() => history.push(routes.signUp)}>cadastrar</div>
-		</>
+		<FormContainer>
+			<FormTitle>Acesse sua conta</FormTitle>
+
+			<FormStyle onSubmit={formSubmit}>
+				<InputLabel htmlFor='email'>E-mail</InputLabel>
+
+				<InputStyle
+					name='email'
+					value={inputs.email}
+					onChange={event =>
+						inputModifier('email', event.target.value)
+					}
+					placeholder='Ex: joao@email.com'
+					type='email'
+					required={true}
+					$loading={loading}
+				/>
+
+				<InputLabel htmlFor='password'>Senha</InputLabel>
+
+				<InputStyle
+					name='password'
+					value={inputs.password}
+					onChange={event =>
+						inputModifier('password', event.target.value)
+					}
+					placeholder='Senha'
+					type='password'
+					required={true}
+					$loading={loading}
+				/>
+
+				<FormButton type='submit' $loading={loading}>
+					Entrar
+				</FormButton>
+			</FormStyle>
+
+			<FakeLink to={routes.signUp} $loading={loading}>
+				Não tem conta? Cadastre-se!
+			</FakeLink>
+		</FormContainer>
 	);
 }
