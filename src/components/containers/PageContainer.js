6a3b08250ee1134229f60ece8e562b6eed28d9a4
Nello-Moreira <g.moreira1@outlook.com>
2021-11-16 01:19:21 -0300

    implemented: order history layout

--- src/components/containers/PageContainer.js
+++ src/components/containers/PageContainer.js
@@ -4,7 +4,7 @@ const PageContainer = styled.div`
 	width: 100%;
 	min-height: calc(100vh - var(--header-height));
 	margin-top: calc(var(--header-height) - 1px);
-	padding: 1px;
+	padding-top: 1px;
 `;
 
 export default PageContainer;
--- src/components/userOrders/UserOrders.js
+++ src/components/userOrders/UserOrders.js
@@ -1,3 +1,64 @@
+import PageContentContainer from '../containers/PageContentContainer';
+import {
+	OrderHistoryContainer,
+	HistoryCard,
+	ImageContainer,
+	SecondaryInfoContainer,
+	ProductName,
+	BuyAgainButton,
+} from './UserOrdersStyles';
+
 export default function UserOrders() {
-	return <div>UserOrders</div>;
+	return (
+		<PageContentContainer>
+			<OrderHistoryContainer>
+				<h2>Histórico de compras</h2>
+
+				<HistoryCard>
+					<ImageContainer>
+						<img
+							src={
+								'https://img.freepik.com/vetores-gratis/astronauta-segurando-a-bandeira-no-espaco-dos-desenhos-animados-icone-ilustracao-icone-do-espaco-de-tecnologia-isolado-estilo-flat-cartoon_138676-3099.jpg?size=338&ext=jpg'
+							}
+							alt='q'
+						/>
+					</ImageContainer>
+					<ProductName>
+						descrição descriçãodescrição descrição descrição
+						descrição descrição descrição descriçãodescrição
+						descrição descrição descrição descrição descrição
+						descriçãodescrição descriçãodescrição descrição
+						descrição descrição descrição descrição
+						descriçãodescrição descrição descrição descrição
+						descrição descrição descriçãodescrição
+						descriçãodescrição descrição descrição descrição
+						descrição descrição descriçãodescrição descrição
+						descrição descrição descrição descrição descrição
+						descrição descriçãodescrição descrição descrição
+						descrição descrição descrição descriçãodescrição
+						descrição descrição descrição descrição descrição
+						descrição descrição descriçãodescrição descrição
+						descrição descrição descrição descrição
+						descriçãodescrição descrição descrição descrição
+						descrição descrição descriçãodescrição
+						descriçãodescrição descrição descrição descrição
+						descrição descrição descriçãodescrição descrição
+						descrição descrição descrição descrição
+						descriçãodescrição descriçãodescrição descrição
+						descrição descrição descrição descrição
+						descriçãodescrição descrição descrição descrição
+						descrição descrição descriçãodescrição
+						descriçãodescrição descrição descrição descrição
+						descrição descrição descriçãodescrição descrição
+						descrição descrição descrição descrição descrição
+					</ProductName>
+					<SecondaryInfoContainer>
+						<div>Quantidade 100</div>
+						<div>Preço unitário R$ 1300</div>
+						<BuyAgainButton>Comprar novamente</BuyAgainButton>
+					</SecondaryInfoContainer>
+				</HistoryCard>
+			</OrderHistoryContainer>
+		</PageContentContainer>
+	);
 }
